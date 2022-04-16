fb2ab30df1caa51f28e4b0904cadcc012f09b4f5
mahmudul1995 <mahmuduls451@gmail.com>
2022-04-17 02:28:35 +0600

    banner commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
 import Header from './Pages/Header/Header';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
       <Header></Header>
       <Routes>
-        
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
       </Routes>
     </div>
   );
---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+import ServicesProvide from '../ServicesProvide/ServicesProvide';
+
+const Home = () => {
+    return (
+        <>
+            <Banner></Banner>
+            <ServicesProvide></ServicesProvide>
+        </>
+    );
+};
+
+export default Home;
