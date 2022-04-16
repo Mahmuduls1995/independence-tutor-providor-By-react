6bbf05fe8fc148416e07de21393b5a1666d2feae
mahmudul1995 <mahmuduls451@gmail.com>
2022-04-17 02:06:37 +0600

    header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Routes } from 'react-router-dom';
 import logo from './logo.svg';
+import Header from './Pages/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { signOut } from 'firebase/auth';
+import auth from '../../firebase.init'
+import logo from '../../images/tutor.png'
+const Header = () => {
+
+    const [user] = useAuthState(auth)
+
+    const handleSignOut = () => {
+        signOut(auth)
+    }
+    return (
+        <>
+
+            <Navbar collapseOnSelect bg="primary" sticky="top" expand="lg">
+                <Container>
+                    <Navbar.Brand as={Link} to="/">
+                        <img style={{ height: '50px', width: '150px' }} src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="m-auto">
+                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2" href="home#services">Services</Nav.Link>
+                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2" href="home#experts">Experts</Nav.Link>
+                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded mx-2" href="home#experts">Blogs</Nav.Link>
+
+                            <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300  rounded" as={Link} to="/about">About</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            {
+                                user ?
+                                    <button className="btn btn-primary" onClick={handleSignOut}>Sign Out</button>
+                                    :
+                                    <Nav.Link className="text-white hover:bg-purple-700 ease-in duration-300 rounded" eventKey={2} as={Link} to="login">
+                                        Login
+                                    </Nav.Link>
+                                    
+                            }
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+        </>
+    );
+};
+
+export default Header;
