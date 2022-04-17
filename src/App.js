f5a44688a16060d37300cfa57c4e085ccfc6c61c
mahmudul1995 <mahmuduls451@gmail.com>
2022-04-17 19:58:22 +0600

    check Out page complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router-dom';
 import logo from './logo.svg';
+import CheckOut from './Pages/CheckOut/CheckOut';
 import Header from './Pages/Header/Header';
 import Home from './Pages/Home/Home/Home';
+import RequiredAuth from './Pages/RequiredAuth/RequiredAuth';
 
 function App() {
   return (
@@ -10,6 +12,12 @@ function App() {
       <Routes>
         <Route path="/" element={<Home></Home>}></Route>
         <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/checkout" element={
+        <RequiredAuth>
+          <CheckOut></CheckOut>
+        </RequiredAuth>
+        }></Route>
+        <Route ></Route>
       </Routes>
     </div>
   );
